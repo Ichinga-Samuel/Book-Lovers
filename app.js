a2399e43f741c730ba3c78701a3253d743f6cea1
Ichinga Samuel <ichingasamuel@gmail.com>
2021-09-08 07:38:16 +0100

    final build

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/books', booksRouter);
 
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
-    res.locals.error = err //req.app.get('env') === 'development'? err: {}
+    res.locals.error = req.app.get('env') === 'development'? err: {}
     res.status(err.status || 500)
     res.render('error')
 })
