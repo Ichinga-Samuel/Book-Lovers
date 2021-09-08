e66cffa4e2aa0d84eb7c4d1f0c43333814ea5c86
Ichinga Samuel <ichingasamuel@gmail.com>
2021-09-08 05:44:17 +0100

    fixed multer storage

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,10 @@ const book = require('../models/books');
 const uploader = require('./utils/uploader');
 
 /* GET books listing. */
-router.get('/', async (req, res) => {
-  let books = await book.find().sort('-likes');
-  res.render('books', {books});
-});
+// router.get('/', async (req, res) => {
+//   let books = await book.find().sort('-likes');
+//   res.render('books', {books});
+// });
 
 router.post('/add', uploader.single('cover'), async (req, res) => {
   try{
--- routes/utils/uploader.js
+++ routes/utils/uploader.js
@@ -4,6 +4,7 @@ const gcs = require('multer-cloud-storage');
 const uploader = multer({
   storage: gcs.storageEngine({
     acl: 'publicRead',
+    keyFilename: './config/booklovers-v1-5606eddf4394.json',
     uniformBucketLevelAccess: false,
     destination: function(req, file, cb){
       let dest = `BookLovers`
